8d28f4dd6a21aeef5a392cf82ff442e14a7a94dd
Shuhaib! <127591148+shuhbap@users.noreply.github.com>
2026-04-23 18:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
 
+const app = express();
+const __path = process.cwd();
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__path, 'public')));
+
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__path, '/public/pair.html'));
+});
+
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(__path, '/public/qr.html'));
+});
+
+let qrCode = require('./qr');
+app.use('/qr-code', qrCode);
+
+let pair = require('./pair');
+app.use('/code', pair);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__path, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:' + PORT);
+/*const originalString = "https://gist.github.com/SKL-86/";
+const encodedBase64 = btoa(originalString);
+console.log(encodedBase64);*/
+});
+
+module.exports = app;
