fb2f043e6880259c125dbb99af24c8b57fc42397
Shuhaib! <127591148+shuhbap@users.noreply.github.com>
2026-04-23 17:38:54 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,1 +1,34 @@
+const fs = require('fs').promises;
+const axios = require('axios');
+require('dotenv').config();
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN || "ghp_DuFCEcEVm3";
+async function upload(FILE_NAME,FILE_PATH) {
+  try {
+    const content = await fs.readFile(FILE_PATH, 'utf8');
 
+    const response = await axios.post(
+      'https://api.github.com/gists',
+      {
+        description: 'creds.json afiyabot',
+        public: false,
+        files: {
+          [FILE_NAME]: { content }
+        }
+      },
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+          'User-Agent': 'Node.js'
+        }
+      }
+    );
+
+    return response.data.html_url;
+  } catch (error) {
+    console.error('Failed to upload gist:', error.response?.data || error.message);
+  }
+}
+
+module.exports = { upload };
+
+Ith enth code aan Entha file create akkumbo name kodukka
